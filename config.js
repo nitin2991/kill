83d995c3535aa67569aed51ceec46f5dfb98330f
nitin2991 <nitin.kamboj510@gmail.com>
2025-03-31 17:48:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ module.exports = {
   telegramBotToken: "7354753658:AAHHWb4uZZ77JT0jmAwnmAwKapb3rAIU0xg", // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
   // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
 
-  adminNamesForTelegramBot: ["NITIN_510"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  adminNamesForTelegramBot: ["Nitin_510"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
   // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
 
-  authorisedUserForTelegramBot: ["NITIN_510"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  authorisedUserForTelegramBot: ["Nitin_510"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
   // username or id of the user who can use the bot.
 };
 
