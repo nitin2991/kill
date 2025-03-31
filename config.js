4d18bfec42ff48a2f47c776d5c9738e4fbc2146f
nitin2991 <nitin.kamboj510@gmail.com>
2025-03-31 15:10:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+// CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+
+module.exports = {
+  numberOfRegisterInOneTime: 6,  // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  // no of registration want to make in one command.
+
+  domains: ["lamoshitt.com", "crazzybabe.com"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  // names of domain for the emails.
+
+  oneTimeMailGen: 10 * 100000,
+
+  telegramBotToken: "7507500449:AAFoRP8kru4IHs7YjpHCqwxJaQMQwQzLoZY", // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+
+  adminNamesForTelegramBot: ["BRAINTREECHECKERBOTCHANNEL"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+
+  authorisedUserForTelegramBot: ["BRAINTREECHECKERBOTCHANNEL"], // CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
+  // username or id of the user who can use the bot.
+};
+
+// CC KILLER BOT - https://t.me/BRAINTREECHECKERBOTCHANNEL
